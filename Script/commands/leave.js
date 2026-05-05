03de89c32afa30e5880773a5e05bf589c6461d2f
MR-JUWEL-CHAT-BOT2026 <miraichatbotjuwel@gmail.com>
2026-05-05 14:39:29 +0600

    📦 Upload via GitDrop

---
+++ Script/commands/leave.js
@@ -0,0 +1,17 @@
+module.exports.config = {
+    name: "leave",
+    version: "1.0.0",
+    hasPermssion: 2,  // শুধুমাত্র admin-level ইউজার চালাতে পারবে
+    credits: "rX Abdullah",
+    description: "Make the bot leave the group",
+    commandCategory: "System", // 🔑 এইটা না থাকলে load হবে না
+    usages: "leave",
+    cooldowns: 5
+};
+
+module.exports.run = async ({ api, event }) => {
+    const { threadID } = event;
+    api.sendMessage("👋 Bot is leaving this group!", threadID, () => {
+        api.removeUserFromGroup(api.getCurrentUserID(), threadID);
+    });
+};
